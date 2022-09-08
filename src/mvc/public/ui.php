ec8a1994890d39751bf2cb94d36318c99655a2e9
nabab <thomas.nabet@gmail.com>
2022-09-08 13:22:25 +0200

    Added defined check before BBN_BASEURL use

--- src/mvc/public/ui.php
+++ src/mvc/public/ui.php
@@ -1,7 +1,7 @@
 <?php
 $ctrl->add('page');
 die();
-if (strpos(BBN_BASEURL, APPUI_I18N_ROOT.'ui/') !== 0) {
+if (defined('BBN_BASEURL') && defined('APPUI_I18N_ROOT') && (strpos(BBN_BASEURL, APPUI_I18N_ROOT.'ui/') !== 0)) {
   $ctrl->setUrl($ctrl->pluginPath().'ui')
        ->setIcon('nf nf-fa-flag')
        ->setColor('orange', '#FFF')
